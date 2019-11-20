ef64fbef7ab9cdc761bf622bc34e16f067b80d45
dobrinov <idobrinov@gmail.com>
2019-11-20 11:10:29 +0200

    commit

--- Functions/arrayDemo.js
+++ Functions/arrayDemo.js
@@ -2,7 +2,7 @@ var stringToNum = ['2', '4', '5'].map(Number);
 console.log(stringToNum);
 
 //filter
-var numberEven = ['2', '4', '7', '8'].map(Number).filter(a => a%2 == 0);
+var numberEven = ['2', '4', '7', '8', '23'].map(Number).filter(a => a%2 == 0);
 console.log(numberEven);
 
 //reduce
